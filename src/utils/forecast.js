c2796d0d7d4ade818768a2d91186479a7b6f9307
unknown <pinlac.johncarlo@gmail.com>
2019-12-28 20:51:51 +0800

    fahr to celcius

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/2c5520515fb9c1b49137a9df76eddde0/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/2c5520515fb9c1b49137a9df76eddde0/' + latitude + ',' + longitude + '?units=si'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress Celcius out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
